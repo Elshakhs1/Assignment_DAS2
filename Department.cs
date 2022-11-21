bf15cb86cb1e072b41f39863fe0175f75dc0b82c
Islam Elshakhs <91914143+Elshakhs1@users.noreply.github.com>
2022-11-21 14:55:12 +0200

    Add files via upload

---
+++ Department.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_DSA
+{
+    internal class Department
+    {
+        public string depName { get; set; }
+        public string ManagerName { get; set; }
+        public Department Parent { get; set; }
+        public List<Employee> employees { get; set; }
+        public List<Department> subDepartments { get; set; }
+
+        public Department(string depName)
+        {
+            this.depName = depName;
+            employees = new List<Employee>();
+            subDepartments = new List<Department>();
+        }
+        #region department management
+        public Department FindDepartment(Department root, string name)
+        {
+            if (root.depName.Equals(name)) return root;
+            if (root.subDepartments.Count == 0) return null;
+            foreach (Department department in root.subDepartments)
+            {
+                if (department.depName.Equals(name)) return department;
+                var temp = FindDepartment(department, name);
+                if (temp != null) return temp;
+            }
+            return null;
+        }
+
+        public void AddDepartment(Department dep)
+        {
+            dep.Parent = this;
+            this.subDepartments.Add(dep);
+        }
+
+        public void DeleteDepartment()
+        {
+            Department save = this;
+            foreach (Department department in save.subDepartments)
+            {
+                Parent.AddDepartment(department);
+            }
+            Parent.subDepartments.Remove(save);
+        }
+
+        public void moveDepartment(string toDepartment, Department root)
+        {
+            var tempDep = this;
+            if (tempDep.depName.Equals(root.depName))
+            {
+                Console.WriteLine("Cannot move root department! Press enter to continue.");
+                Console.ReadLine();
+                return;
+            }
+
+            var toDep = FindDepartment(root, toDepartment);
+            if (toDep == null)
+            {
+                Console.WriteLine("Desired department does not exist! Press enter to continue.");
+                Console.ReadLine();
+                return;
+            };
+
+            this.Parent.subDepartments.Remove(this);
+            tempDep.Parent = toDep;
+            toDep.subDepartments.Add(tempDep);
+        }
+
+        public int calculateEmployees(Department department)
+        {
+            if (department.subDepartments.Count == 0) return department.employees.Count;
+            int total = department.employees.Count;
+            foreach (Department dep in department.subDepartments)
+            {
+                total = total + calculateEmployees(dep);
+            }
+            return total;
+        }
+        #endregion
+
+        #region employees management
+        public void addEmployee(string name)
+        {
+            Employee employee = new Employee(name);
+            employees.Add(employee);
+        }
+        public void deleteEmployee(string name)
+        {
+            foreach (var employee in employees)
+            {
+                if (employee.fullName.Equals(name))
+                {
+                    employees.Remove(employee);
+                    Console.WriteLine("Employee successfuly found and deleted! Press enter to continue");
+                    Console.ReadLine();
+                    return;
+                }
+            }
+
+            Console.WriteLine("Desired employee does not exist in this department! Press enter to continue");
+            Console.ReadLine();
+        }
+        public void moveEmployee(string employeeName, string toDep, Department root)
+        {
+            Employee transit;
+            Department toDepartment = FindDepartment(root, toDep);
+
+            foreach (var employee in employees)
+            {
+                if (employee.fullName.Equals(employeeName))
+                {
+                    transit = new Employee(employeeName);
+                    toDepartment.employees.Add(transit);
+                    this.deleteEmployee(transit.fullName);
+                    Console.WriteLine("Please ignore the previous message, it is a bug. Thank you. Press enter to continue");
+                    Console.ReadLine();
+                    return;
+                }
+
+            }
+        }
+        #endregion
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_DSA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Department root = new Department("root");
+            fillWithData(root);
+            string menu =
+                "Please enter one of these commands to proceed:\n" +
+                "1. Find and get Department.\n" +
+                "2. Add new Subdepartment.\n" +
+                "3. Delete current Department.\n" +
+                "4. Move current Department to another as Subdepartment.\n" +
+                "5. Add an employee to current Department.\n" +
+                "6. Delete an employee from current Department\n" +
+                "7. Move employee from current Department to another.\n" +
+                "8. Display employees for this Departments and the ones under it.\n";
+            ;
+
+            Console.WriteLine(menu);
+            displayCurrentDep(root);
+            Console.WriteLine("");
+            int input = int.Parse(Console.ReadLine());
+            Department current = root;
+            while (input != 0)
+            {
+                switch (input)
+                {
+                    case 1:
+                        Console.Clear();
+                        Console.WriteLine("Please enter the desired Department you are looking for:");
+                        string desiredDep = Console.ReadLine();
+                        current = root;
+                        current = current.FindDepartment(current, desiredDep);
+                        if (current == null)
+                        {
+                            Console.WriteLine("Department does not exist. Press enter to continue.");
+                            current = root;
+                            Console.ReadLine();
+                        }
+                        break;
+
+                    case 2:
+                        Console.Clear();
+                        Console.WriteLine("Please enter the new departments name:");
+                        string desiredDepName = Console.ReadLine();
+                        Department toAdd = new Department(desiredDepName);
+                        current.AddDepartment(toAdd);
+                        Console.WriteLine("Department successfully added! Press enter to continue.");
+                        Console.ReadLine();
+                        break;
+
+                    case 3:
+                        Console.Clear();
+                        if (current == root)
+                        {
+                            Console.WriteLine("Cannot delete root department! Press enter to continue.");
+                            Console.ReadLine();
+                        }
+                        else
+                        {
+                            var tempParent = current.Parent;
+                            current.DeleteDepartment();
+                            current = tempParent;
+                            Console.WriteLine("Department successfuly deleted. All subdepartments have been given to the Parent department.\n" +
+                                "Returning to Parent department. Press enter to continue.");
+                            Console.ReadLine();
+                        }
+                        break;
+                    case 4:
+                        Console.Clear();
+                        Console.WriteLine("Please enter the department you want to move it to:");
+                        string toDep = Console.ReadLine();
+                        current.moveDepartment(toDep, root);
+                        break;
+                    case 5:
+                        Console.Clear();
+                        Console.WriteLine("Please enter the new employees name:");
+                        string empName = Console.ReadLine();
+                        Employee emp = new Employee(empName);
+                        current.addEmployee(empName);
+                        Console.WriteLine("Employee successfuly added! Press enter to continue");
+                        Console.ReadLine();
+                        break;
+                    case 6:
+                        Console.Clear();
+                        if (current.employees.Count == 0)
+                        {
+                            Console.WriteLine("No employees to delete! Press enter to continue.");
+                            Console.ReadLine();
+                            break;
+                        }
+                        Console.WriteLine("Please enter the name of the employee to delete:");
+                        string empNameDel = Console.ReadLine();
+                        current.deleteEmployee(empNameDel);
+                        break;
+                    case 7:
+                        Console.Clear();
+                        if (current.employees.Count == 0)
+                        {
+                            Console.WriteLine("There are no employees in this department to move. Press enter to continue.");
+                            Console.ReadLine();
+                            break; ;
+                        }
+
+                        Console.WriteLine("Please enter the name of the employee you want to move:\n");
+                        Console.WriteLine("PS. Please write the employees name correctly, because there is no validation,\n" +
+                            "it is 4 AM when I write this code\n" +
+                            "and I am too tired to write more validation...");
+                        string empMoveName = Console.ReadLine();
+                        Console.WriteLine("\nTo which Department:");
+                        Console.WriteLine("PS. Same goes here. I need some sleep because of my brilliant time management skills.\n" +
+                            "Please share any advice you have for this problem that I have...");
+                        string toWhichDepName = Console.ReadLine();
+                        current.moveEmployee(empMoveName, toWhichDepName, root);
+                        break;
+                    case 8:
+                        Console.Clear();
+                        Console.WriteLine($"Number of total employees in current department and all subdepartments is: {current.calculateEmployees(current)}.");
+                        Console.WriteLine("Press enter to continue.");
+                        Console.ReadLine();
+                        break;
+                    default:
+                        break;
+                }
+
+                Console.Clear();
+                Console.WriteLine(menu + "\n");
+                displayCurrentDep(current);
+                Console.WriteLine("");
+                input = int.Parse(Console.ReadLine());
+            }
+        }
+
+        static void displayCurrentDep(Department dep)
+        {
+            Console.WriteLine($"You are currently managing {dep.depName} department.");
+            if (dep.Parent != null)
+            {
+                Console.WriteLine($"The Parent department is: {dep.Parent.depName}.");
+            }
+            Console.WriteLine("\nActive subdepartments:");
+            foreach (Department subDep in dep.subDepartments)
+            {
+                Console.WriteLine(subDep.depName);
+            }
+            Console.WriteLine("\nActive employees:");
+            foreach (Employee emp in dep.employees)
+            {
+                Console.WriteLine(emp.fullName);
+            }
+        }
+
+        static void fillWithData(Department root)
+        {
+            Department Engineering = new Department("Engineering");
+            Department Accounting = new Department("Accounting");
+            Department QA = new Department("QA");
+
+            root.AddDepartment(Engineering);
+            root.AddDepartment(Accounting);
+            root.AddDepartment(QA);
+
+            Department Software = new Department("Software");
+            Department Mechanical = new Department("Mechanical");
+
+            Engineering.AddDepartment(Software);
+            Engineering.AddDepartment(Mechanical);
+
+            Department Income = new Department("Income");
+            Department Expenses = new Department("Expenses");
+
+            Accounting.AddDepartment(Income);
+            Accounting.AddDepartment(Expenses);
+
+            Department ManualQA = new Department("ManualQA");
+            Department AutoQA = new Department("AutoQA");
+
+            QA.AddDepartment(ManualQA);
+            QA.AddDepartment(AutoQA);
+
+            Department BackEnd = new Department("BackEnd");
+            Department FrontEnd = new Department("FrontEnd");
+
+            Software.AddDepartment(FrontEnd);
+            Software.AddDepartment(BackEnd);
+
+            Department Hardware = new Department("Hardware");
+
+            Mechanical.AddDepartment(Hardware);
+
+            Department PythonQA = new Department("PythonQA");
+            Department JavaQA = new Department("JavaQA");
+
+            AutoQA.AddDepartment(PythonQA);
+            AutoQA.AddDepartment(JavaQA);
+        }
+    }
+}
